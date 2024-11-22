f67ba452f4b7bd7d6d426af40619c0acbcd2d922
mirandalucas52 <mirandalucas52@gmail.com>
2024-11-22 12:24:48 +0100

    feat: extract text from PDF

--- upload.py
+++ upload.py
@@ -1,4 +1,5 @@
 import requests
+import PyPDF2
 
 def convert_pdf_to_text():
     file_id = "1-lEpAcgZ7B05yVJUK4OJyEdB9t7x031i"
@@ -10,3 +11,14 @@ def convert_pdf_to_text():
         print("File downloaded successfully!")
     else:
         print("Failed to download the file. Status code:", response.status_code)
+    
+    file_path = "output.pdf"
+    if file_path:
+        with open(file_path, 'rb') as pdf_file:
+            pdf_reader = PyPDF2.PdfReader(pdf_file)
+            num_pages = len(pdf_reader.pages)
+            text = ''
+            for page_num in range(num_pages):
+                page = pdf_reader.pages[page_num]
+                text += page.extract_text() + " "
+            print("Text extracted:", text)
